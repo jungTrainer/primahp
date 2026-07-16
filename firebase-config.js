73fda9961f34c7994ba1cc4a4ca7143fc5b24379
Jungkyungmun <93896901+jungTrainer@users.noreply.github.com>
2026-07-16 16:21:00 +0900

    Load editable content enhancements

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,14 @@
 // 브라우저용 Firebase 공개 설정만 불러옵니다.
 if (!window.__firebase_config) {
-  document.write('<script src="firebase-public-config.js?v=20260716-8"><\/script>');
+  document.write('<script src="firebase-public-config.js?v=20260716-9"><\/script>');
 }
 (() => {
   const page = location.pathname.split('/').pop() || 'index.html';
-  const load = src => { const s=document.createElement('script');s.src=`${src}?v=20260716-8`;s.defer=true;document.head.appendChild(s); };
+  const load = src => { const s=document.createElement('script');s.src=`${src}?v=20260716-9`;s.defer=true;document.head.appendChild(s); };
   if (page === 'index.html') {
     window.addEventListener('load',()=>{
       load('assets/ga4-loader.js');
+      load('assets/site-content-enhancements.js');
       if (new URLSearchParams(location.search).get('preview') === '1') load('assets/site-editor-ui.js');
     },{once:true});
   }
